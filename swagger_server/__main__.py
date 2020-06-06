7f8f07a203ae8a5853883b8eac49f8c3f4e35161
Daniel-Gisolfi <Daniel.Gisolfi@ibm.com>
2020-06-06 12:56:50 -0400

    Added CORS

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -1,19 +1,15 @@
 #!/usr/bin/env python3
 
 import connexion
-
+from flask_cors import CORS 
 from swagger_server import encoder
 
 
 def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
+    CORS(app.app)
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'DgisolfiAPI'}, pythonic_params=True)
-    @app.after_request
-    def add_headers(response):
-        response.headers.add('Access-Control-Allow-Origin', '*')
-        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-        
     app.run(port=8080, debug=False)
 
 
