5124e2fe76c23e9a05bad0d644e30f23beec227f
Daniel-Gisolfi <Daniel.Gisolfi@ibm.com>
2020-06-06 12:51:05 -0400

    Added allow origin

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -9,6 +9,11 @@ def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'DgisolfiAPI'}, pythonic_params=True)
+    @app.after_request
+    def add_headers(response):
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+        
     app.run(port=8080, debug=False)
 
 
